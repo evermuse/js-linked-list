e7250fa769db79716dbf6404da87504afaa1e7d2
Bryan Alexander <bryanalexander@me.com>
2015-09-04 14:26:27 -1000

    created newGetListInfo module. stuck on how to access linkedList

--- linkedList.js
+++ linkedList.js
@@ -3,6 +3,69 @@
  * @description  Main Module
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
-function linkedListGenerator(){
+function linkedListGenerator() {
+
+  newLinkedList = {
+
+  value: 'Ready Player One',
+  next: {
+    value: '1982',
+    next: {
+      value: 'Neuromancer',
+      next: {
+        value: 'Snow Crash',
+        next: null
+      }
+    }
+  }
+
+  };
+
+  newGetListInfo = {
+
+    getHead : function() {
+
+
+
+    },
+
+    getTail : function() {
+
+    },
+
+    add : function(value) {
+
+
+
+    },
+
+    remove : function(value) {
+
+    },
+
+    get : function(number) {
+
+    },
+
+    insert : function(number) {
+
+    }
+
+
+  };
+
+  return newGetListInfo;
+
+}
+
+
+function createNode( animal ) {
+
+  return {
+
+    value: animal,
+    next: null
+
+  }
 
 }
